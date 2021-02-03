d59d6e85178cef1b6e82bef7869342c30f5b7bc6
Steven Kemp <github2@steven.globaloffset.com>
2021-02-03 18:19:30 -0500

    Three-headed hydra
    1. Add scaleX support for Google Meet to keep it from undoing the flipping
    2. Add a limit to the mutation observer to try to detect infinite loops
    3. Add support to be kinder to other properties on the transform style

--- flip.js
+++ flip.js
@@ -1,6 +1,17 @@
 (function() {
+    const DEBUG_MODE = false;
+    const MUTATION_OBSERVER_MAX_COUNT = 1000;
 
-    const DEBUG_MODE = true;
+    DEBUG_MODE && console.log('VideoMirror script loaded!');
+
+    // Prevent duplicate instances of VideoMirror from loading.
+    if (window.videoMirrorIsLoaded === true) {
+        DEBUG_MODE && console.log('Anti-double loading activated.');
+        return;
+    }
+    window.videoMirrorIsLoaded = true;
+
+    window.videoMirrorIsFlipped = false;
 
     const flipDuration = 250;
     const flipQuery = [
@@ -11,12 +22,32 @@
         'iframe[src^="https://player.vimeo.com"]',       // Embedded vimeo
     ].join(', ');  // (note: this must also be changed in background.js)
 
-    let watchList = [];
+    let watchList = new Map();
 
-    window.videoMirrorIsFlipped = false;
+    // Detect short-circuits within VideoMirror. If two instances are running,
+    // this counter keeps track of how many times the mutation observer has been called.
+    // If it is called too many times it is disconnected.
+    let mutationObserverCount = 0;
 
     function flipVideo(video, animate, flip) {
         if (typeof flip === 'undefined') flip = true; // Flip defaults to true
+
+
+        if (mutationObserverCount > MUTATION_OBSERVER_MAX_COUNT) {
+            let observer = watchList.get(video);
+
+            if (typeof observer != 'undefined') {
+                observer.disconnect();
+                DEBUG_MODE && console.log('Mutation observer disconnected. Too many calls');
+            }
+
+        }
+        mutationObserverCount++;
+
+        // Set a timer to reduce the mutation observer count.
+        setTimeout(() => {
+            mutationObserverCount -= 2;
+        }, 500);
 
         if (animate) {
             video.style.transition = 'transform ' + flipDuration + 'ms ease-out';
@@ -26,12 +57,53 @@
             }, flipDuration);
         }
 
-        video.style.transform = flip ? 'rotateY(180deg)' : 'rotateY(0deg)';
+        // Make VideoMirror compatible with other transforms
+        let transformString = video.style.transform;
+        let transformArray = video.style.transform.split(' ');
+        let newTransformArray = [];
+
+        DEBUG_MODE && console.log('This is what VM saw: ' + transformString);
+
+        let flippedFromLoop = false;
+        for (transformComponent of transformArray) {
+            if (transformComponent.includes('rotateY')) {
+                newTransformArray.push(flip ? 'rotateY(180deg)' : 'rotateY(0deg)');
+                flippedFromLoop = true;
+            } else if (transformComponent.includes('scaleX') && window.location.host == 'meet.google.com'){
+                // Google Meet uses a scale transform to flip the video for the user.
+                // This keeps that so pressing the button doesn't appear to do nothing.
+            } else {
+                newTransformArray.push(transformComponent);
+            }
+        }
+
+        if (window.location.host == 'meet.google.com') {
+            // Google Meet uses a scale transform to flip the video for the user.
+            // This keeps that so pressing the button doesn't appear to do nothing.
+            newTransformArray.push('scaleX(-1)');
+        }
+
+
+        if (flippedFromLoop) {
+            video.style.transform = newTransformArray.join(' ');
+            DEBUG_MODE && console.log('This is what VM changed it to: ' + newTransformArray.join(' '));
+        } else {
+            DEBUG_MODE && console.log('This is what VM changed it to: ' + video.style.transform + (flip ? 'rotateY(180deg)' : 'rotateY(0deg)'));
+            video.style.transform = video.style.transform + (flip ? 'rotateY(180deg)' : 'rotateY(0deg)');
+        }
 
         // Keep watch over the video if it is flipped, otherwise stop keeping watch of it.
-        if (watchList.indexOf(video) == -1) {
-            observeVideo(video);
-            watchList.push(video);
+        if (flip) {
+            if (typeof watchList.get(video) == 'undefined') {
+                let observer = observeVideo(video);
+                watchList.set(video, observer);
+            }
+        } else {
+            let observer = watchList.get(video);
+
+            if (typeof observer != 'undefined') {
+                observer.disconnect();
+            }
         }
     }
 
@@ -41,12 +113,15 @@
                 if (mutation.type == 'attributes' && mutation.attributeName == 'style') {
                     // The style attribute was updated on this element.
                     // We should make sure that our transforms are still there.
+                    DEBUG_MODE && console.log('Mutation Observer called');
                     flipVideo(mutation.target, false, window.videoMirrorIsFlipped);
                 }
             });
         });
 
         observer.observe(video, { attributes: true });
+
+        return observer;
     }
 
     function videoMirrorSetFlip(flipped) {
